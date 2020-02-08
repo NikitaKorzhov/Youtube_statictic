0d2fe5b1f68f59db33a8ac5191f66dfa0ce500d1
NikitaKorzhov <korzhovnikita@gmail.com>
2020-02-08 18:56:02 +0200

    second v

--- script.js
+++ script.js
@@ -18,18 +18,14 @@ $(document).ready(()=>{
       });
 
       chanalInfo= async function(id){
-        t= await gapi.client.youtube.channels.list({
-          "part": "snippet,contentDetails,statistics",
-          "maxResults": 50,
-          "id": id
-        }).then(response=>response.result.items)
-        return t
-      }
+        let request_params={"part": "snippet,contentDetails,statistics","maxResults": 50,"id": id}
+        return await gapi.client.youtube.channels.list(request_params).then(response=>response.result.items)
+    }
     push= function(item){
       return {chanal:item.channelTitle,description:item.description_c,channelId:item.channelId,subscribers:item.subscriber_c,picture:item.picture_c,count:1,url:`https://www.youtube.com/channel/${item.channelId}`}
     }  
      var ff=function(items){
-    u=[]
+    let  u=[]
     items.map(  el=>{
       if(u.some(ii=>ii.channelId===el.snippet.channelId)){
         u.map(it=>it.channelId===el.snippet.channelId?it.count++:"")
@@ -44,15 +40,11 @@ $(document).ready(()=>{
     }
 
       fragen=async function(j){
-        it_u=[]
+        let leked_videos=[]
         npt=""
-        params={"part": "snippet,contentDetails,statistics",
-          "maxResults": 50,
-          "myRating": "like",
-          }
+        params={"part": "snippet,contentDetails,statistics","maxResults": 50,"myRating": "like"}
         for (i=0;i<j;i++){
           npt!=""?params.pageToken=npt:"";
-          console.log(params)
           result=await gapi.client.youtube.videos.list(params).then(response=>response.result);
           npt=result.nextPageToken;     
           o=""
@@ -68,28 +60,17 @@ $(document).ready(()=>{
                 
                 })
           }
-          result.items.map(y=>it_u.push(y))  
+          result.items.map(y=>leked_videos.push(y))  
         }
-    
-        return it_u;
+        return leked_videos;
       }   
       function execute() {
         return fragen(5)
             .then(function(response) {
-                    // Handle the results here (response.result has the parsed body).
-                    s= ff(response)
+                    let s= ff(response)
                     s.forEach(element => element.persent=element.count*100/response.length);
-                   s.map(t=>$("#list1").append(`<p>
-                   <div class="row">
-                   <div class="col-sm-4"><img src="${t.picture}" style="width:88px;height:88px;" class="rounded-circle" alt="Cinque Terre" width="304" height="236"> </div>
-                   <div class="col-sm-8"> <h4 ><a href="${t.url}">${t.chanal} - ${t.count} лайков</h4></a><p>${t.subscribers} поодписчиков</p></div></div>
-                      <div>${t.description}</div>
-                      <div class="progress">
-      <div class="progress-bar" role="progressbar" style="width: ${t.persent}%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">${parseInt(t.persent)}%</div>
-    </div>
-                      </p>`));
-                        response.map(item=>$("#list").append(`<p>${item.snippet.title}</p>`))
-                    
+                    s.map(el=>$("#channelTemplate").tmpl(el).appendTo("#list1"));
+                   response.map(item=>$("#list").append(`<p>${item.snippet.title}</p>`));    
                   },
                   function(err) { console.error("Execute error", err); });
       }
